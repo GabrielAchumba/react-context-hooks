63623dcd88d33634ee4438093ac8d4535800891b
GabrielAchumba <achumba.gabriel@yahoo.com>
2023-11-03 15:45:27 +0100

    updates

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,31 +2,36 @@ import React, { Component } from 'react';
 import { ThemeContext } from '../contexts/ThemeContext';
 
 class Navbar extends Component {
-    static contextType = ThemeContext
+    //static contextType = ThemeContext
     
     render() {
-
-        const { 
-            isLghtTheme,
-            light,
-            dark
-         } = this.context;
-
-         const theme = isLghtTheme ? light : dark
-
-         const navBarStyle = {
-            background: theme.ui,
-            color: theme.syntax
-         }
         return (
-            <nav style={navBarStyle}>
-                <h1>Context App</h1>
-                <ul>
-                    <li>Home</li>
-                    <li>About</li>
-                    <li>Contact</li>
-                </ul>
-            </nav>
+            <ThemeContext.Consumer>
+                {(context) => {
+                    const { 
+                        isLghtTheme,
+                        light,
+                        dark
+                     } = context;
+            
+                     const theme = isLghtTheme ? light : dark
+            
+                     const navBarStyle = {
+                        background: theme.ui,
+                        color: theme.syntax
+                     }
+                    return (
+                        <nav style={navBarStyle}>
+                            <h1>Context App</h1>
+                            <ul>
+                                <li>Home</li>
+                                <li>About</li>
+                                <li>Contact</li>
+                            </ul>
+                        </nav>
+                    )
+                }}
+            </ThemeContext.Consumer>
         );
     }
 }
