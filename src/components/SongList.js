c1ee3236a0f0856728280bb5c709973fadc18b15
GabrielAchumba <achumba.gabriel@yahoo.com>
2023-11-03 16:55:11 +0100

    updates

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,10 +1,31 @@
+import { useState } from 'react';
+import { v1 as uuidv1 } from 'uuid';
+
+
 const SongList = () => {
+
+    const [songs, setSongs] = useState([
+        { title: 'almost home', id: 1 },
+        { title: 'memory gospel', id: 2 },
+        { title: 'this wild darkness', id: 3 }
+    ])
+
+    const addSong = () => {
+        setSongs([
+            ...songs, 
+            {title: 'new song', id: uuidv1()}
+        ])
+    }
     return ( 
         <div className="song-list">
             <ul>
-                <li>this wild darkness</li>
-                <li>memory gospel</li>
+                {
+                    songs.map((song) => {
+                        return (<li key={song.id}>{song.title}</li>)
+                    })
+                }
             </ul>
+            <button onClick={addSong} >Add a song</button>
         </div>
      );
 }
