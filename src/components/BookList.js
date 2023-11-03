4b582a73b5f34d2b564e09c4870fddde76912948
GabrielAchumba <achumba.gabriel@yahoo.com>
2023-11-03 15:49:54 +0100

    updates

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -2,32 +2,40 @@ import React, {Component} from 'react';
 import { ThemeContext } from '../contexts/ThemeContext';
 
 class BookList extends Component {
-    static contextType = ThemeContext
+    //static contextType = ThemeContext
     render() { 
-        const { 
-            isLghtTheme,
-            light,
-            dark
-         } = this.context;
-
-        const theme = isLghtTheme ? light : dark
-
-        const BookListStyle = {
-            background: theme.bg,
-            color: theme.syntax
-        }
-
-        const ListItemStyle = {
-            background: theme.ui
-        }
+        
         return (
-            <div className="book-list" style={BookListStyle} >
-                <ul>
-                    <li style={ListItemStyle}>the way of kings</li>
-                    <li style={ListItemStyle}>the name of the wind</li>
-                    <li style={ListItemStyle}>the final empire</li>
-                </ul>
-            </div>
+            <ThemeContext.Consumer>
+                {(context) => {
+                    const { 
+                        isLghtTheme,
+                        light,
+                        dark
+                     } = context;
+            
+                    const theme = isLghtTheme ? light : dark
+            
+                    const BookListStyle = {
+                        background: theme.bg,
+                        color: theme.syntax
+                    }
+            
+                    const ListItemStyle = {
+                        background: theme.ui
+                    }
+
+                    return (
+                        <div className="book-list" style={BookListStyle} >
+                            <ul>
+                                <li style={ListItemStyle}>the way of kings</li>
+                                <li style={ListItemStyle}>the name of the wind</li>
+                                <li style={ListItemStyle}>the final empire</li>
+                            </ul>
+                        </div>
+                    )
+                }}
+            </ThemeContext.Consumer>
         );
     }
 }
